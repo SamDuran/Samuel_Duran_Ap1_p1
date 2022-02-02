53e2dce9fadc4279c6d12da39fedefdc7ded0e9d
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-01 22:59:01 -0400

    Personalizando MainWindow

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,17 +12,28 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using Samuel_Duran_Ap1_p1_.UI.Consulta;
+//using Samuel_Duran_Ap1_p1_.UI.Registro;
 
 namespace Samuel_Duran_Ap1_p1_
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //Primer parcial aplicada 1. Samuel Duran 2019-0793 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void rMenuItem_Click(object sander, RoutedEventArgs e){
+            //var registro = new Registro();
+            //registro.Show();
+        }
+        private void cMenuItem_Click(object sander, RoutedEventArgs e){
+            //Consulta consulta = new Consulta();
+            //consulta.Show();
         }
     }
 }
