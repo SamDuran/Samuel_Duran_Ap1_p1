7d767a1b75177818bd5b3de9c3d18e0e04adcae8
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-02 00:01:55 -0400

    Agregando UI de registro

---
+++ UI/Registros/Registro.xaml.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Samuel_Duran_Ap1_p1_.UI{
+    public partial class Registro : Window{
+        public Registro()
+        {
+            InitializeComponent();
+        }
+        private void Limpiar(){
+
+        }
+
+        private bool Validar(){
+            bool esValido = false;
+
+            return esValido;
+        }
+        private void Guardar(){
+
+        }
+        private void Cargar(){
+
+        }
+        private void buscarBtn_Click(object sander, RoutedEventArgs e){
+
+        }
+        private void guardarBtn_Click(object sander, RoutedEventArgs e){
+
+        }
+        private void eliminarBtn_Click(object sander, RoutedEventArgs e){
+
+        }
+        private void nuevoBtn_Click(object sander, RoutedEventArgs e){
+
+        }
+        private void eliminar(){
+
+        }
+    }
+}
