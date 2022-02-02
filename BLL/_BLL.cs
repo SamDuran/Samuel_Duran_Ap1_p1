9806c6f6b4903e4ece59484a6eb98f25c91a3242
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-01 21:40:48 -0400

    Agregando BLL

---
+++ BLL/_BLL.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Samuel_Duran_Ap1_p1_.BLL{
+    public class _BLL{
+    }
+}
