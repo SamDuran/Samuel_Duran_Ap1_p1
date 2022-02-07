8a98c310d96d9d3fdafebe57d2be537232e125a3
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-07 17:33:18 -0400

    Reparando problema menor

--- BLL/_BLL.cs
+++ BLL/_BLL.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
 
 namespace Samuel_Duran_Ap1_p1_.BLL{
     public class _BLL{
--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 public class Contexto : DbContext
 {
-    protected override void onConfiguring(DbContextOptionsBuilder optionsBuilder){
-        optionsBuilder.UseSqlite(@"DataSource=DATA/Samuel_Duran_Ap1_p1.db");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+        optionsBuilder.UseSqlite(@"Data Source=DATA/Samuel_Duran_Ap1_p1.db");
     }
 }
