fdb2537677dc9b193e82d871c507cdd1c8733597
SamuelDurán <94940561+elSamu19@users.noreply.github.com>
2022-02-07 19:02:58 -0400

    Indicando namespace de Entidades

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,12 +1,15 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
-public partial class Productos
+namespace Samuel_Duran_Ap1_p1_.Entidades
 {
-    [Key]
-    public int ProductoId { get; set; }
-    public string? Descripcion { get; set; }
-    public bool Existencia { get; set; }
-    public decimal Costo { get; set; }
-    public decimal ValorInventario { get; set; }
+    public partial class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string? Descripcion { get; set; }
+        public bool Existencia { get; set; }
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+    }
 }
